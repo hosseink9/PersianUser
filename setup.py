6595b655444df58681f249c9ee93fe5dbd489393
Hossein Kalantari <kalantari@hojangroup.com>
2025-05-20 14:26:01 +0330

    Fix code

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="PersianUser",
+    name="persianuser",
     version="0.1.0",
     author="Hossein Kalantari",
-    author_email="kalandevwork@gmail    .com",
+    author_email="kalandevwork@gmail.com",
     description="A Python library for creating and validating Persian user information",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/hosseink9/PersianUser",
     packages=find_packages(),
@@ -15,6 +15,8 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=3.8",
 )
